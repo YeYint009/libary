80398012aa5f5f81ec2f4663c5128f3fca9c5bac
Ye Yint <blackskym96@gmail.com>
2023-12-05 21:56:49 +0900

    yyhdev

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+import glide from '@glidejs/glide'
 import './style.css'
+
+const glideOptions = {
+    type:'carousel',
+    perView : 3,
+    autoplay : 4000,
+    breakpoints: {
+        1024: {
+          perView: 3
+        },
+        600: {
+          perView: 1
+        }
+    }
+}
+new glide(".glide",glideOptions).mount();
